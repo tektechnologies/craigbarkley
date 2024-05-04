19730fe0445dc137c46bd0f665ff13a8585ed3a8
tektechnologies <info@tektechnologies.com>
2024-05-04 18:51:52 -0500

    side bar functionality added

--- src/components/DeveloperManagerArtist/DeveloperManagerArtist.js
+++ src/components/DeveloperManagerArtist/DeveloperManagerArtist.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Dropdown from '../'
 
 class DeveloperManagerArtist extends React.Component {
 
---
+++ src/components/DropdownNav/DropdownNav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import styled from '@emotion/styled';
+
+const SiderBar = styled.div`
+  background: #151418;
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  z-index: 999;
+  transition: 0.3s ease-in-out;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+
+
+`
+
+
+class DropdownNav extends React.Component {
+  render() {
+    return (
+      <div>DropdownNav</div>
+    )
+  }
+}
+
+export default DropdownNav;
