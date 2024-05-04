02a1d87b59aab43664975441612412466394a1f2
tektechnologies <info@tektechnologies.com>
2024-05-04 15:22:39 -0500

    add client work import

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DeveloperManagerArtist from '../components/DeveloperManagerArtist/DeveloperManagerArtist';
-
+import ClientWork from '../components/ClientWork';
 
 class HomePage extends Component {
   render() {
