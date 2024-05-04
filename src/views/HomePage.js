008deeb30cc8d0dad896fc21800262c1e055c8f5
tektechnologies <info@tektechnologies.com>
2024-05-04 15:34:51 -0500

    add imports for HomePage component return

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import DeveloperManagerArtist from '../components/DeveloperManagerArtist/DeveloperManagerArtist';
 import ClientWork from '../components/ClientWork';
+import About from '../components/About/About';
+import Contact from '../components/Contact/Contact';
+import Footer from '../components/Footer/Footer';
+import SocialIcon from '../components/SocialIcon/SocialIcon';
+import ScrollToTop from '../components/SocialIcon/ScrollToTop';
+
+
+
 
 class HomePage extends Component {
   render() {
