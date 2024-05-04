185abac33fb7ddb668c916c8262694edda6e3c96
tektechnologies <info@tektechnologies.com>
2024-05-04 15:20:26 -0500

    add first 2 component folders for client work and introduction

---
+++ src/views/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import DeveloperManagerArtist from '';
+
+class HomePage extends Component {
+  render() {
+    return (
+     <>
+      <DeveloperManagerArtist />
+      <ClientWork />
+      <About />
+      <Contact />
+      <Footer />
+      <ScrollToTop />
+     </>
+    )
+  }
+}
