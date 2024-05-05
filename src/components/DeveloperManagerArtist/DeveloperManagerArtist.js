2e43d8f3779e5405cb3360c919fdc664f512883c
tektechnologies <info@tektechnologies.com>
2024-05-04 19:20:52 -0500

    dropdown and header added to dev manager artist component

--- src/components/DeveloperManagerArtist/DeveloperManagerArtist.js
+++ src/components/DeveloperManagerArtist/DeveloperManagerArtist.js
@@ -1,15 +1,38 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Dropdown from '../DropdownNav/DropdownNav';
+import Header from '../Header/Header'
 
 
 class DeveloperManagerArtist extends React.Component {
 
+constructor(props){
+  super(props);
+  this.state ={
+    isOpen: false,
+    showSubtitle: false,
+    showScrollDown: false,
+  };
+}
+
+
+toggle = () => {
+   this.setState((prevState) => ({
+    isOpen: !prevState.isOpen,
+   }));
+  };
 
 
   render() {
-   
+  
+
+    const {isOpen, showSubtitle, showScrollDown} = this.state;
+
+
     return (
-      
+      <main>
+        <Dropdown isOpen={isOpen} toggle={this.toggle} />
+        <Header toggle={this.toggle} />
+      </main>
     )
   }
 }
