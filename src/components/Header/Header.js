63e4e1d7c7366de1fc7980953a06ca39c7723b09
tektechnologies <info@tektechnologies.com>
2024-05-08 20:44:42 -0500

    add nav elements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import {Nav,Logo, NavLink, Bars, NavMenu, NavBtn} from './HeaderElements';
+import React from "react";
+import { Nav, Logo, NavLink, Bars, NavMenu, NavBtn } from "./HeaderElements";
 
 class Header extends React.Component {
-
   render() {
-
     return (
-
       <div>
-      Nav header
+        <Nav>
+          <Logo>
+            <img />
+          </Logo>
+          <NavMenu>
+            <NavLink>Projects</NavLink>
+            <NavLink>About</NavLink>
+            <NavLink>Contact</NavLink>
+          </NavMenu>
+          <NavBtn>
+            <a>Resume</a>
+          </NavBtn>
+          <Bars />
+        </Nav>
       </div>
-    )
+    );
   }
 }
-
 
 export default Header;
