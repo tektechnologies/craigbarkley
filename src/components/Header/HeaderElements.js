5aa49f9a05f268c9906189d67ce9ab303aa8ef72
tektechnologies <info@tektechnologies.com>
2024-05-08 20:02:05 -0500

    add Nav style

---
+++ src/components/Header/HeaderElements.js
@@ -0,0 +1,19 @@
+import { FaBars } from 'react-icons/fa';
+import { Link as ScrollLink } from 'react-scroll';
+import styled from '@emotion/styled';
+
+
+export const Nav = styled.nav`
+  background: transparent;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 10;
+`;
+
+// export const = styled.``;
+// export const = styled.``;
+// export const = styled.``;
+// export const = styled.``;
+// export const = styled.``;
