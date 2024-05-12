b92ff1137f9f4609e069a160cd5e27dccee1f4ba
tektechnologies <info@tektechnologies.com>
2024-05-12 18:45:53 -0500

    add containers for main section

--- src/components/DeveloperManagerArtist/DeveloperManagerArtist.js
+++ src/components/DeveloperManagerArtist/DeveloperManagerArtist.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Dropdown from '../DropdownNav/DropdownNav';
-import Header from '../Header/Header';
+import React from "react";
+import Dropdown from "../DropdownNav/DropdownNav";
+import Header from "../Header/Header";
 import {
   DmaContainter,
   DmaWrapper,
@@ -9,45 +9,42 @@ import {
   Image,
   ScrollDown,
   ScrollLink,
-} from './DevElements';
-
+} from "./DevElements";
 
 class DeveloperManagerArtist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      showSubtitle: false,
+      showScrollDown: false,
+    };
+  }
 
-constructor(props){
-  super(props);
-  this.state ={
-    isOpen: false,
-    showSubtitle: false,
-    showScrollDown: false,
+  toggle = () => {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen,
+    }));
   };
-}
-
-
-toggle = () => {
-   this.setState((prevState) => ({
-    isOpen: !prevState.isOpen,
-   }));
-  };
-
 
   render() {
-  
     // showSubtitle, showScrollDown ~ WIP
-    const {isOpen, } = this.state;
-    console.log(this.state)
+    const { isOpen } = this.state;
+    console.log(this.state);
 
     return (
       <main>
         <Dropdown isOpen={isOpen} toggle={this.toggle} />
         <Header toggle={this.toggle} />
         <DmaContainter>
-
+          <DmaWrapper>
+            <DmaLeft></DmaLeft>
+            <DmaRight></DmaRight>
+          </DmaWrapper>
         </DmaContainter>
       </main>
-    )
+    );
   }
 }
-
 
 export default DeveloperManagerArtist;
