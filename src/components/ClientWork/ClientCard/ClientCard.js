7747de3ccfa52e1ddab323ea0cd23366d734ec6d
tektechnologies <info@tektechnologies.com>
2024-05-16 19:55:33 -0500

    add client card, work component, and import

---
+++ src/components/ClientWork/ClientCard/ClientCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+ class ClientCard extends Component {
+  render() {
+    return (
+      <div>ClientCard</div>
+    )
+  }
+}
+
+
+export default ClientCard;
---
+++ src/components/ClientWork/ClientWork.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ClientCard from './ClientCard/ClientCard';
+
+
+
+
+class ClientWork extends Component {
+  render() {
+    return (
+      <>
+        <div>ClientWork</div>
+        <ClientCard />
+      </>
+      
+    )
+  }
+}
+
+
+export default ClientWork;
