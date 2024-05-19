3370d2d7d1e59244e27e5c8f9cd3c76a0f70a7e7
tektechnologies <info@tektechnologies.com>
2024-05-19 15:23:09 -0500

    add card h4 and p styles

--- src/components/ClientWork/ClientCard/ClientCardElements.js
+++ src/components/ClientWork/ClientCard/ClientCardElements.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 //https://emotion.sh/docs/styled
- 
+
 export const Card = styled.div`
   display: grid;
   grid-gap: 2rem;
@@ -9,8 +9,9 @@ export const Card = styled.div`
   padding-bottom: 2rem;
   overflow: hidden;
   border-radius: 10px;
-  box-shadow: 0px 5px 15px -3px rgba(0,0,0,0.1),0 4px 6px -2px rgba(0,0,0,0.05);
-  @media (min-width: 992px){
+  box-shadow: 0px 5px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
     border-bottom: 0;
     padding-bottom: 0;
@@ -20,7 +21,7 @@ export const Card = styled.div`
 export const CardLeft = styled.div`
   justify-self: center;
   height: 100%;
-  img{
+  img {
     object-fit: cover;
   }
 `;
@@ -29,4 +30,23 @@ export const CardRight = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  h4 {
+    font-size: 1.5rem;
+    font-weight: 400;
+  }
+  p {
+    font-weight: 400;
+    max-width: 95%;
+    margin-top: 10px;
+    margin-bottom: 1rem;
+    color: rgba(0, 0, 0, 0.815);
+    text-align: center;
+    @media (min-width: 992px) {
+      text-align: start;
+    }
+  }
+  @media (min-width: 992px) {
+    align-items: flex-start;
+    margin-top: 1rem;
+  }
 `;
