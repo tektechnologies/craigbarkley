a76c25df74e47face239eb2cf4677eac363b5018
tektechnologies <info@tektechnologies.com>
2024-05-19 14:56:29 -0500

    add one project for render testing

---
+++ src/data/ClientData.js
@@ -0,0 +1,13 @@
+// Client and Project list
+export const ClientList = [
+  {
+    img: "/art-sticker.jpg",
+    title: "Art Pennington",
+    description:
+      "Art Pennington (1923-2017) was a legendary baseball player whose achievements transcended the diamond.",
+    tech_stack: ["DNS Setup", "Email Setup", "SEO", "Wix Hosting"],
+    github_url: "https://www.artpenningtonbaseball.com/",
+    demo_url: "https://www.artpenningtonbaseball.com/",
+  },
+
+]
