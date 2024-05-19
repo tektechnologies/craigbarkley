27c6ba030517617e0ebf71648c355e9034006ac1
tektechnologies <info@tektechnologies.com>
2024-05-19 15:45:00 -0500

    update data url order

--- src/data/ClientData.js
+++ src/data/ClientData.js
@@ -6,7 +6,7 @@ export const ClientList = [
     description:
       "Art Pennington (1923-2017) was a legendary baseball player whose achievements transcended the diamond.",
     tech_stack: ["DNS Setup", "Email Setup", "SEO", "Wix Hosting"],
-    github_url: "https://www.artpenningtonbaseball.com/",
+    github_url: "https://tektechnologies.github.io/Art-Pennington-Baseball/",
     demo_url: "https://www.artpenningtonbaseball.com/",
   },
 
