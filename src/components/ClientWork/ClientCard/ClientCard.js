97d05c778db0b2c47aef6e0d5668ac81bad03101
tektechnologies <info@tektechnologies.com>
2024-05-19 15:32:33 -0500

    add imports for animation and card styles

--- src/components/ClientWork/ClientCard/ClientCard.js
+++ src/components/ClientWork/ClientCard/ClientCard.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { ClientList } from '../../../data/ClientData';
-
+import {
+  Card,
+  CardLeft,
+  CardRight,
+  TechCardContainer,
+  TechCard,
+  BtnGroup,
+} from './ClientCardElements';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 
 
