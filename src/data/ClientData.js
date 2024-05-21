b77c4dc78151685327498af0ff6add2a717eb045
tektechnologies <info@tektechnologies.com>
2024-05-20 21:07:10 -0500

    add SAFE project data

--- src/data/ClientData.js
+++ src/data/ClientData.js
@@ -9,5 +9,14 @@ export const ClientList = [
     github_url: "https://github.com/tektechnologies/Art-Pennington-Baseball",
     demo_url: "https://www.artpenningtonbaseball.com/",
   },
+  {
+    img: "/SAFE_Logo.png",
+    title: "SAFE",
+    description:
+      "Our mission is to ban fracking and other fossil fuel extraction in, and shipment of fossil fuels through, Southern Illinois.",
+    tech_stack: ["DNS Setup", "Email Setup", "SEO", "Wix Hosting", "Virus Removal","User Training on Internet Products"],
+    github_url: "https://github.com/tektechnologies/SAFE-fracking-site",
+    demo_url: "https://www.dontfractureillinois.org/",
+  },
 
 ]
