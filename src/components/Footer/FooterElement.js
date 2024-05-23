a54207947228f4a838753b7fac27c777335b9404
tektechnologies <info@tektechnologies.com>
2024-05-22 19:39:03 -0500

    footer image and styles

---
+++ src/components/Footer/FooterElement.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+
+export const FooterSection = styled.div`
+background-image: url(/footer_wave.svg);
+background-repeat: no-repeat;
+background-size: cover;
+width: 100%;
+height: 250px;
+position: relative;
+
+
+`;
