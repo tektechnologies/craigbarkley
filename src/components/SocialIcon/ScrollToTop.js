57b4aaa28ab912537d269e3417bd3bdfc4441acc
tektechnologies <info@tektechnologies.com>
2024-05-23 21:12:42 -0500

    add handleclick and bind in state

--- src/components/SocialIcon/ScrollToTop.js
+++ src/components/SocialIcon/ScrollToTop.js
@@ -1,8 +1,25 @@
 import React  from 'react';
 import { StyledIconButton, StyledarrowIcon } from './StyleElements';
-
+import { animateScroll } from 'react-scroll';
 
  class ScrollToTop extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      open: false,
+      shouldRender: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({open: false});
+    animateScroll.scrollToTop({ duration: 0});
+  }
+
+
+
   render() {
     return (
       <div>ScrollToTop</div>
