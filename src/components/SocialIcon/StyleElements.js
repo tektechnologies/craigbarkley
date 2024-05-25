fafc34ebe1975744ef698bb57c97b7f4fc9c1fbe
tektechnologies <info@tektechnologies.com>
2024-05-25 17:41:18 -0500

    add container style rules

--- src/components/SocialIcon/StyleElements.js
+++ src/components/SocialIcon/StyleElements.js
@@ -9,11 +9,39 @@ const SocialContainer = styled.div`
   transform: translateY(-50%);
 
   ul {
+    display: block;
   }
   .item + .item {
+    margin-top: 1rem;
   }
   a {
+    font-size: 2.3rem;
+    color: rgb(119,119,121);
     &:hover {
+      color: rgb(57,134,250);
+    }
+  }
+  @media screen and (max-width: 1000px){
+    margin-top: 2rem;
+    position: relative;
+    top: 0;
+    left: 0;
+    ul {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      list-style: none;
+    }
+    a{
+      font-size: 2.5rem;
+      color: #151418;
+      &:hover {
+        color: rgb(57, 134, 250);
+      }
+    }
+    .item + .item {
+      margin-top: 0;
+      margin-left: 2rem;
     }
   }
 `;
