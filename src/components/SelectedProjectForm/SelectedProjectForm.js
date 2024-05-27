bf6b206163ef27d282ab74e99f202a8cc1581e43
tektechnologies <info@tektechnologies.com>
2024-05-27 18:07:22 -0500

    add html form elements basic

--- src/components/SelectedProjectForm/SelectedProjectForm.js
+++ src/components/SelectedProjectForm/SelectedProjectForm.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 
 const SelectedProjectForm = (props) => {
   console.log("props in form comp: ", props);
+
   return (
     <Modal show={props.show} onHide={props.handleClose}>
       <Modal.Dialog>
@@ -13,32 +14,68 @@ const SelectedProjectForm = (props) => {
         </Modal.Header>
 
         <Modal.Body>
+          <form>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" name="firstName" />
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" name="lastName" />
+            <label htmlFor="companyName">Company</label>
+            <input type="text" id="companyName" name="companyName" />
+            <label htmlFor="emailAddress">Email</label>
+            <input type="text" id="emailAddress" name="emailAddress" />
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input type="text" id="phoneNumber" name="phoneNumber" />
+            <label htmlFor="visitForeign">
+              Have you visited a foreign country in the last 7 days
+            </label>
+            <input type="checkbox" id="visitForeign" name="visitForeign" />
+            <hr />
+            <label htmlFor="visitForeign">Visit Reason</label>
+            <input
+              type="textarea"
+              rows="4"
+              cols="50"
+              id="visitForeign"
+              name="visitForeign"
+            />
+            <hr />
+            <label htmlFor="toolsRequired">Tools Required?</label>
+            <input type="checkbox" id="toolsRequired" name="toolsRequired" />
+            <hr />
+            <label htmlFor="toolReasons">Reasons For Tooling</label>
+            <input type="textarea" id="toolReasoned" name="toolReasoned" />
+            <hr />
+            <label htmlFor="visitDate">Visit Date</label>
+            <input type="date" id="visitDate" name="visitDate" />
+            <label htmlFor="startTime">Start Time</label>
+            <input type="time" id="startTime" name="startTime" />
+            <label htmlFor="endTime">End Time</label>
+            <input type="time" id="endTime" name="endTime" />
+            <label htmlFor="userLocation">Your Location</label>
+            <input type="time" id="userLocation" name="userLocation" />
+            <label for="category">Category</label>
 
-        <form>
-          
-        </form>
-        {/* firstName
-        lastName
-        company
-        email
-        phone number
-        Have you visited a foreign country in the last 7 days (yes/no)
-        <hr>
-        vitsitReason - text box big
-        <hr>
-        toolsRequired (yes/no)
-        Reason for tools text box big
-        <hr>
-        Visit details calendar 
-        Start time
-        End time
-        Your Location
-        Category
-        <p>No cell phones</p>
-        signature box */}
-
+            <select name="category" id="category">
+              <option selected disabled>
+                Choose Project
+              </option>
+              <option value="webSite">Web Site</option>
+              <option value="seo">Digital Marketing</option>
+              <option value="onlineStore">Online Store</option>
+              <option value="logoBranding">Logo Graphics Branding</option>
+            </select>
+          </form>
         </Modal.Body>
         <Modal.Footer>
+          <p>
+            The content and design of this website are provided for
+            informational purposes only and are created by a professional web
+            designer. While every effort is made to ensure the accuracy and
+            quality of the information and design elements, the website designer
+            makes no guarantees, representations, or warranties regarding the
+            currentness, completeness, or suitability of the content for any
+            specific purpose.
+          </p>
           <Button onClick={props.handleClose} variant="secondary">
             Close
           </Button>
