4748d3c528cab62a4c1ae05de7551bb128381077
tektechnologies <info@tektechnologies.com>
2024-05-27 16:53:21 -0500

    add form outline and form tags

--- src/components/SelectedProjectForm/SelectedProjectForm.js
+++ src/components/SelectedProjectForm/SelectedProjectForm.js
@@ -12,7 +12,32 @@ const SelectedProjectForm = (props) => {
           <Modal.Title>Project Form</Modal.Title>
         </Modal.Header>
 
-        <Modal.Body></Modal.Body>
+        <Modal.Body>
+
+        <form>
+          
+        </form>
+        {/* firstName
+        lastName
+        company
+        email
+        phone number
+        Have you visited a foreign country in the last 7 days (yes/no)
+        <hr>
+        vitsitReason - text box big
+        <hr>
+        toolsRequired (yes/no)
+        Reason for tools text box big
+        <hr>
+        Visit details calendar 
+        Start time
+        End time
+        Your Location
+        Category
+        <p>No cell phones</p>
+        signature box */}
+
+        </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.handleClose} variant="secondary">
             Close
