5c6061a391a4b458efb65cc36fe9a743da2470fa
tektechnologies <info@tektechnologies.com>
2024-05-29 11:24:45 -0500

    remove bootstrap styling

--- src/components/SelectedProjectForm/SelectedProjectForm.js
+++ src/components/SelectedProjectForm/SelectedProjectForm.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
@@ -48,163 +44,116 @@ const SelectedProjectForm = (props) => {
         </Modal.Header>
 
         <Modal.Body>
-          <Container>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Row>
-                <Col xs={4} md={4}>
-                  <label htmlFor="firstName">First Name</label>
-                  <input
-                    type="text"
-                    id="firstName"
-                    {...register("firstName")}
-                  />
-                </Col>
-                <Col xs={4} md={4}>
-                  <label htmlFor="lastName">Last Name</label>
-                  <input type="text" id="lastName" {...register("lastName")} />
-                </Col>
-                <Col xs={4} md={4}>
-                  <label htmlFor="companyName">Company</label>
-                  <input
-                    type="text"
-                    id="companyName"
-                    {...register("companyName")}
-                  />
-                </Col>
-                <Col xs={6} md={4}>
-                  <label htmlFor="emailAddress">Email</label>
-                  <input
-                    type="text"
-                    id="emailAddress"
-                    {...register("emailAddress")}
-                  />
-                </Col>
-                <Col xs={6} md={4}>
-                  <label htmlFor="phoneNumber">Phone Number</label>
-                  <input
-                    type="text"
-                    id="phoneNumber"
-                    {...register("phoneNumber")}
-                  />
-                </Col>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" {...register("firstName")} />
 
-                <Col xs={12} md={8}>
-                  <label htmlFor="visitCompetion">
-                    Have you contacted another provider in the last 7 days?
-                  </label>
-                  <input
-                    type="checkbox"
-                    id="visitCompetion"
-                    {...register("visitCompetion")}
-                  />
-                </Col>
-              </Row>
-              <hr />
-              <Row>
-                <Col xs={12} md={8}>
-                  <label htmlFor="visitReason">Visit Reason</label>
-                  <input
-                    type="textarea"
-                    id="visitReason"
-                    {...register("visitReason")}
-                  />
-                </Col>
-                <hr />
-                <Col xs={12} md={8}>
-                  <label htmlFor="instructionRequired">
-                    Product Instruction Required?
-                  </label>
-                  <input
-                    type="checkbox"
-                    id="instructionRequired"
-                    {...register("instructionRequired")}
-                  />
-                </Col>
-                <hr />
-                <Col xs={12} md={8}>
-                  <label htmlFor="instructWhat">Instruction Specifics</label>
-                  <input
-                    type="textarea"
-                    id="instructWhat"
-                    {...register("instructWhat")}
-                  />
-                </Col>
-              </Row>
-              <hr />
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" {...register("lastName")} />
 
-              <Row>
-                <Col xs={4} md={4}>
-                  <label htmlFor="visitDate">Visit Date</label>
-                  <input
-                    type="date"
-                    id="visitDate"
-                    {...register("visitDate")}
-                  />
-                </Col>
-                <Col xs={4} md={4}>
-                  <label htmlFor="startTime">Start Time</label>
-                  <input
-                    type="time"
-                    id="startTime"
-                    {...register("startTime")}
-                  />
-                </Col>
-                <Col xs={4} md={4}>
-                  {" "}
-                  <label htmlFor="endTime">End Time</label>
-                  <input type="time" id="endTime" {...register("endTime")} />
-                </Col>
-                <Col xs={6} md={4}>
-                  <label htmlFor="userLocation">Your Location</label>
-                  <input
-                    type="time"
-                    id="userLocation"
-                    {...register("userLocation")}
-                  />
-                </Col>
-                <Col xs={6} md={4}>
-                  <label htmlFor="productcategory">Category</label>
+            <label htmlFor="companyName">Company</label>
+            <input type="text" id="companyName" {...register("companyName")} />
 
-                  <select
-                    id="category"
-                    defaultValue={"DEFAULT"}
-                    {...register("productcategory")}
-                  >
-                    <option value="DEFAULT" disabled>
-                      Choose Project
-                    </option>
-                    <option value="webSite">Web Site</option>
-                    <option value="seo">Digital Marketing</option>
-                    <option value="onlineStore">Online Store</option>
-                    <option value="logoBranding">Logo Graphics Branding</option>
-                  </select>
-                </Col>
-              </Row>
-              <Row>
-                <button>Submit</button>
-              </Row>
-            </form>
-            <DevTool control={control} />
-          </Container>
+            <label htmlFor="emailAddress">Email</label>
+            <input
+              type="text"
+              id="emailAddress"
+              {...register("emailAddress")}
+            />
+
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input type="text" id="phoneNumber" {...register("phoneNumber")} />
+
+            <label htmlFor="visitCompetion">
+              Have you contacted another provider in the last 7 days?
+            </label>
+            <input
+              type="checkbox"
+              id="visitCompetion"
+              {...register("visitCompetion")}
+            />
+
+            <hr />
+
+            <label htmlFor="visitReason">Visit Reason</label>
+            <input
+              type="textarea"
+              id="visitReason"
+              {...register("visitReason")}
+            />
+
+            <hr />
+
+            <label htmlFor="instructionRequired">
+              Product Instruction Required?
+            </label>
+            <input
+              type="checkbox"
+              id="instructionRequired"
+              {...register("instructionRequired")}
+            />
+
+            <hr />
+
+            <label htmlFor="instructWhat">Instruction Specifics</label>
+            <input
+              type="textarea"
+              id="instructWhat"
+              {...register("instructWhat")}
+            />
+
+            <hr />
+
+            <label htmlFor="visitDate">Visit Date</label>
+            <input type="date" id="visitDate" {...register("visitDate")} />
+
+            <label htmlFor="startTime">Start Time</label>
+            <input type="time" id="startTime" {...register("startTime")} />
+
+            <label htmlFor="endTime">End Time</label>
+            <input type="time" id="endTime" {...register("endTime")} />
+
+            <label htmlFor="userLocation">Your Location</label>
+            <input
+              type="time"
+              id="userLocation"
+              {...register("userLocation")}
+            />
+
+            <label htmlFor="productcategory">Category</label>
+
+            <select
+              id="category"
+              defaultValue={"DEFAULT"}
+              {...register("productcategory")}
+            >
+              <option value="DEFAULT" disabled>
+                Choose Project
+              </option>
+              <option value="webSite">Web Site</option>
+              <option value="seo">Digital Marketing</option>
+              <option value="onlineStore">Online Store</option>
+              <option value="logoBranding">Logo Graphics Branding</option>
+            </select>
+
+            <button>Submit</button>
+          </form>
+          <DevTool control={control} />
         </Modal.Body>
         <Modal.Footer>
-          <Row>
-            <Col xs={12} md={8}>
-              <p>
-                The content and design of this website are provided for
-                informational purposes only and are created by a professional
-                web designer. While every effort is made to ensure the accuracy
-                and quality of the information and design elements, the website
-                designer makes no guarantees, representations, or warranties
-                regarding the currentness, completeness, or suitability of the
-                content for any specific purpose.
-              </p>
-            </Col>
-          </Row>
+          <p>
+            The content and design of this website are provided for
+            informational purposes only and are created by a professional web
+            designer. While every effort is made to ensure the accuracy and
+            quality of the information and design elements, the website designer
+            makes no guarantees, representations, or warranties regarding the
+            currentness, completeness, or suitability of the content for any
+            specific purpose.
+          </p>
 
-          <Button onClick={props.handleClose} variant="secondary">
+          <button onClick={props.handleClose} variant="secondary">
             Close
-          </Button>
+          </button>
         </Modal.Footer>
       </Modal.Dialog>
     </Modal>
