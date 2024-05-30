2675ce191d07dd8fa65a9ef50f8203085061e893
tektechnologies <info@tektechnologies.com>
2024-05-30 10:28:12 -0500

    add format remove error passing child

--- src/components/SelectedProjectForm/SelectedProjectForm.js
+++ src/components/SelectedProjectForm/SelectedProjectForm.js
@@ -3,8 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "react-bootstrap/Modal";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 // type FormValues = {
 //   firstName : string,
 //   lastName : string,
@@ -112,14 +112,11 @@ const SelectedProjectForm = (props) => {
                     {...register("visitReason")}
                   /> */}
                   <textarea
-                    placeholder="Enter text here"
                     htmlFor="visitReason"
                     id="visitReason"
                     rows="4"
                     {...register("visitReason")}
-                  >
-                    {""}
-                  </textarea>
+                  ></textarea>
                 </div>
                 <hr
                   style={{
@@ -150,14 +147,11 @@ const SelectedProjectForm = (props) => {
                 {...register("toolReason")}
               /> */}
                   <textarea
-                    placeholder="Enter text here"
                     htmlFor="toolReason"
                     id="toolReason"
                     rows="4"
                     {...register("toolReason")}
-                  >
-                    {""}
-                  </textarea>
+                  ></textarea>
                 </div>
 
                 <hr
@@ -171,7 +165,7 @@ const SelectedProjectForm = (props) => {
                 <div className="h2-blocklevel">
                   <h2>Visit Details</h2>
                 </div>
-                <div class="label-input-group three-col">
+                <div className="label-input-group three-col">
                   <label htmlFor="visitDate">Visit Date</label>
                   <input
                     type="date"
@@ -180,7 +174,7 @@ const SelectedProjectForm = (props) => {
                   />
                 </div>
 
-                <div class="label-input-group three-col">
+                <div className="label-input-group three-col">
                   <label htmlFor="startTime">Start Time</label>
                   <input
                     type="time"
@@ -189,7 +183,7 @@ const SelectedProjectForm = (props) => {
                   />
                 </div>
 
-                <div class="label-input-group three-col">
+                <div className="label-input-group three-col">
                   <label htmlFor="endTime">End Time</label>
                   <input type="time" id="endTime" {...register("endTime")} />
                 </div>
@@ -225,17 +219,22 @@ const SelectedProjectForm = (props) => {
                     border: "1px solid grey",
                   }}
                 />
-                
-                <FontAwesomeIcon icon={faCircleExclamation} style={{color: "#E75B29",}} />                <h2 className="h2-blocklevel color-alert">ELECTRONIC DEVICES</h2>
-          <p>
-            No cell phones, cameras, audio or visual equipment, or any other
-            electronic devices are permitted on the premises without written
-            approval by Door Access System. By signing this statement, you
-            declare that you have no electronic devices including those
-            mentioned above on your person, and you have no intention of
-            bringing such devices onto the premises without prior written
-            approval
-          </p>
+
+                <FontAwesomeIcon
+                  icon={faCircleExclamation}
+                  style={{ height: "40px", color: "#E75B29" }}
+                />
+                <h2 className="color-alert">ELECTRONIC DEVICES</h2>
+
+                <p>
+                  No cell phones, cameras, audio or visual equipment, or any
+                  other electronic devices are permitted on the premises without
+                  written approval by Door Access System. By signing this
+                  statement, you declare that you have no electronic devices
+                  including those mentioned above on your person, and you have
+                  no intention of bringing such devices onto the premises
+                  without prior written approval
+                </p>
                 <button className="next-button">Next</button>
               </form>
             </div>
@@ -243,8 +242,6 @@ const SelectedProjectForm = (props) => {
           <DevTool control={control} />
         </Modal.Body>
         <Modal.Footer>
-         
-
           <button onClick={props.handleClose} variant="secondary">
             Close
           </button>
